38079efaf2b243b4dac81d1310c9f31c82b15361
meinzeug <dennis.wicht@web.de>
2025-07-26 15:38:00 +0200

    Update index.tsx

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,1 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { AuthProvider } from './hooks/useAuth';
+import DebugApiView from './components/views/DebugApiView';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
 
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
+}
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/debug-api" element={<DebugApiView />} />
+        <Route
+          path="*"
+          element={
+            <AuthProvider>
+              <App />
+            </AuthProvider>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
